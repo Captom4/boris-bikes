469823a73affce1817ee94a381287863973d4193
Tom Hird <tombjh@hotmail.co.uk>
2018-10-03 13:51:48 +0000

    Completed chapter 14 complex attributes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,10 @@
+require 'docking_station'
+
 describe DockingStation do
-  # other tests omitted for brevity
 
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
