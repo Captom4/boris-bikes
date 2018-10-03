87d51959df2649707a0789cef1f2fea9b58183ee
Tom Hird <tombjh@hotmail.co.uk>
2018-10-03 20:03:05 +0000

    Step 17 initialization defaults

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,34 @@
+require_relative 'bike'
+
 class DockingStation
+  attr_accessor :capacity
+  
+  DEFAULT_CAPACITY = 20
+  
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
+  end
+  
+  
   def initialize
     @bikes = []
   end
-
+  
   def release_bike
     fail 'No bikes available' if @bikes.empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if @bikes.count >= DEFAULT_CAPACITY
     @bikes << bike
   end
+  
   private
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
