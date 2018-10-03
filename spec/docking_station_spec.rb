868bfea0540cc559a3ccba9541d39098c98c3af9
Tom Hird <tombjh@hotmail.co.uk>
2018-10-03 20:02:21 +0000

    Step 17 initialization defaults

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,10 +5,19 @@ require 'Bike'
     
     describe '#dock' do
     it 'error will will display when full' do
-    DockingStation::DEFAULT_CAPACITY.times do
-      subject.dock Bike.new
-    end
+    subject.capacity.times { subject.dock Bike.new }
     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    
   end
+ end
 end
+describe 'initialization' do
+  subject { DockingStation.new }
+  let(:bike) { Bike.new }
+  it 'defaults capacity' do
+    described_class::DEFAULT_CAPACITY.times do
+      subject.dock(bike)
+    end
+    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+  end
 end
