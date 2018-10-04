ea9e278059a7b3aefbf7b3625d884c9e4a8a742b
Tom Hird <tombjh@hotmail.co.uk>
2018-10-04 14:25:17 +0000

    part 20 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,22 +2,32 @@ require 'DockingStation'
 require 'Bike'
 
   describe DockingStation do
-    
-    describe '#dock' do
-    it 'error will will display when full' do
-    subject.capacity.times { subject.dock Bike.new }
-    expect { subject.dock Bike.new }.to raise_error 'Docking station full'
-    
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    subject.dock Bike.new
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
- end
+
+  it 'does not release broken bikes' do
+  bike = double(:bike, broken?: true)
+  subject.dock bike
+  expect {subject.release_bike}.to raise_error 'No bikes available'
 end
-describe 'initialization' do
-  subject { DockingStation.new }
-  let(:bike) { Bike.new }
-  it 'defaults capacity' do
-    described_class::DEFAULT_CAPACITY.times do
-      subject.dock(bike)
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'releases working bikes' do
+  bike = double(:bike, broken?: false)
+  subject.dock bike
+  expect(subject.release_bike).to be bike
+end
+
+  describe 'dock' do
+    it 'raises an error when full' do
+      subject.capacity.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
-    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
   end
 end
